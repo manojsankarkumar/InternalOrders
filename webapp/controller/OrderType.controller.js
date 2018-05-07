cb8030ace94f1d8e1dfe5409a688ffd1d49c09b7
Manoj Kumar Sankar Kumar <manojsankarkumar@gmail.com>
2018-05-07 08:52:10 +0000

    1.Icon Tab Bar is working perfectly while entering value in order type and selecting value from value help

--- webapp/controller/OrderType.controller.js
+++ webapp/controller/OrderType.controller.js
@@ -1,48 +1,50 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/m/MessageBox",
-    "InternalOrders/model/formatter"
+	"InternalOrders/model/formatter"
 ], function(Controller, MessageBox, formatter) {
 	"use strict";
 
 	return Controller.extend("InternalOrders.controller.OrderType", {
-      	formatter : formatter,
-			onInit: function(oEvent) {
-		 this.getView().addStyleClass(sap.ui.Device.support.touch ? "sapUiSizeCozy" : "sapUiSizeCompact");
-		 
-		 var no =this.getView().byId("tel").getValue();
-          if(no === ""){
-              sap.m.MessageBox.alert("Please write your Phone Number");
-              return false;
-    }
-          else if(no.length !== 10){
-          sap.m.MessageBox.alert(" enter valid number");
-          return false;
-    }
-    
-    var nos =this.getView().byId("tele").getValue();
-          if(nos === ""){
-              sap.m.MessageBox.alert("Please write your Phone Number");
-              return false;
-    }
-          else if(isNaN(no) || no.length === 10){
-          sap.m.MessageBox.alert(" Enter valid number");
-          return false;
-    }
-			},
+		formatter: formatter,
+		onInit: function(oEvent) {
+			this.getView().addStyleClass(sap.ui.Device.support.touch ? "sapUiSizeCozy" : "sapUiSizeCompact");
+
+			var oIconTab = this.getView().byId("idIconTabBarNoIcons");
+			oIconTab.setVisible(false);
 			
-			handleSave: function(oEvent){
-					this.getOwnerComponent().getRouter().navTo("OrderType");
-			},
-			
-			handleCancel: function(oEvent){
-				
-			},
-			
-			 ValueHelp: function(oEvent) {
-		    this.inputId = oEvent.getSource().getId();
+			var no = this.getView().byId("tel").getValue();
+			if (no === "") {
+				sap.m.MessageBox.alert("Please write your Phone Number");
+				return false;
+			} else if (no.length !== 10) {
+				sap.m.MessageBox.alert(" enter valid number");
+				return false;
+			}
+
+			var nos = this.getView().byId("tele").getValue();
+			if (nos === "") {
+				sap.m.MessageBox.alert("Please write your Phone Number");
+				return false;
+			} else if (isNaN(no) || no.length === 10) {
+				sap.m.MessageBox.alert(" Enter valid number");
+				return false;
+			}
+
+		},
+
+		handleSave: function(oEvent) {
+			this.getOwnerComponent().getRouter().navTo("OrderType");
+		},
+
+		handleCancel: function(oEvent) {
+
+		},
+
+		ValueHelp: function(oEvent) {
+			this.inputId = oEvent.getSource().getId();
 			var output = this.inputId.split("--");
-			var fieldname = output[2]; 
+			var fieldname = output[2];
 			// create value help dialog
 			if (!this._valueHelpDialog) {
 				this._valueHelpDialog = sap.ui.xmlfragment(
@@ -51,18 +53,17 @@ sap.ui.define([
 				);
 				this.getView().addDependent(this._valueHelpDialog);
 			}
-			
-			var fil = new sap.ui.model.Filter("FieldID", sap.ui.model.FilterOperator.EQ, fieldname);  
+
+			var fil = new sap.ui.model.Filter("FieldID", sap.ui.model.FilterOperator.EQ, fieldname);
 			this._valueHelpDialog.getBinding("items").filter([fil]);
-			
+
 			// open project select dialog 
-	//		var selectDialog = this.byId("valuehelpfragment", "valueHelpSelectDialog");
-	        
-	        var resourceBundle = this.getView().getModel("i18n").getResourceBundle();
+			//		var selectDialog = this.byId("valuehelpfragment", "valueHelpSelectDialog");
+
+			var resourceBundle = this.getView().getModel("i18n").getResourceBundle();
 			this._valueHelpDialog.setTitle(formatter.getValueHelpTitle(resourceBundle, fieldname));
 			this._valueHelpDialog.open();
-			
-			
+
 		},
 
 		//Close the project dialog box
@@ -72,16 +73,32 @@ sap.ui.define([
 				var productInput = this.getView().byId(this.inputId);
 				productInput.setValue(oSelectedItem.getTitle());
 			}
-		//	evt.sap.ui.getCore().byId("items");
+			var output = this.inputId.split("--");
+			var fieldname = output[2];
+			if(fieldname === 'AUART'){
+				this.toggleIconTabVisibility(oSelectedItem.getTitle());
+			}
+			//	evt.sap.ui.getCore().byId("items");
+		},
+
+        	toggleIconTabVisibility: function(value){
+			var a = value.length;
+			var oIconTab = this.getView().byId("idIconTabBarNoIcons");
+			if (a > 0) {
+				oIconTab.setVisible(true);
+			} else {
+				oIconTab.setVisible(false);
+			}
 		},
 		
-		orderTypeUpdate: function(evt){
+		orderTypeUpdate: function(evt) {
 			var a = evt.getParameters();
 			a = a.value;
-			alert(a);
+			this.toggleIconTabVisibility(a);
 		}
 		
-		
-});
+	
+
+	});
 
 });
