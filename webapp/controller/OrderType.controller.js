a38c7796474f04ed11fd534b9aa67a21056715ae
Manoj Kumar Sankar Kumar <manojsankarkumar@gmail.com>
2018-05-16 05:59:21 +0000

    Required fields to submit Coding done to create entry in the backend,

--- webapp/controller/OrderType.controller.js
+++ webapp/controller/OrderType.controller.js
@@ -12,29 +12,107 @@ sap.ui.define([
 
 			var oIconTab = this.getView().byId("idIconTabBarNoIcons");
 			oIconTab.setVisible(false);
-			
-			var no = this.getView().byId("tel").getValue();
-			if (no === "") {
-				sap.m.MessageBox.alert("Please write your Phone Number");
-				return false;
-			} else if (no.length !== 10) {
-				sap.m.MessageBox.alert(" enter valid number");
-				return false;
-			}
-
-			var nos = this.getView().byId("tele").getValue();
-			if (nos === "") {
-				sap.m.MessageBox.alert("Please write your Phone Number");
-				return false;
-			} else if (isNaN(no) || no.length === 10) {
-				sap.m.MessageBox.alert(" Enter valid number");
-				return false;
-			}
-
 		},
 
 		handleSave: function(oEvent) {
-			this.getOwnerComponent().getRouter().navTo("OrderType");
+			var compcode = this.getView().byId("BUKRS").getValue();
+			var plant = this.getView().byId("WERKS").getValue();
+			var curr = this.getView().byId("WAERS").getValue();
+			if (compcode === "") {
+				sap.m.MessageBox.alert("Please select Company Code");
+				return true;
+			} else if (plant === "") {
+				sap.m.MessageBox.alert("Please select plant");
+				return true;
+			} else if (curr === "") {
+				sap.m.MessageBox.alert("Please select currency under Control Data");
+				return true;
+			}
+
+			this.getView().getModel().setDeferredGroups(["createGroup"]);
+			this.createEntity();
+
+			this.getView().getModel().submitChanges({
+				groupId: "createGroup",
+				success: function(oData, oResponse) {
+					var data = oData.__batchResponses[0];
+					data = data.__changeResponses[0];
+					data = data.data;
+					var order = data.Order;
+					if (order === "") {
+						sap.m.MessageBox.alert(data.Message);
+					} else {
+						sap.m.MessageBox.alert(order + "Order Created");
+					}
+				},
+				error: function(oError, oResponse) {
+
+					sap.m.MessageBox.alert("Failure");
+				}
+			});
+
+		},
+
+		onAfterRendering: function(oEvent) {
+			var oModel = this.getView().getModel();
+			oModel.attachMetadataLoaded(this.createEntity.bind(this));
+		},
+
+		createEntity: function(oEvent) {
+
+			var oContext = this.getView().getModel().createEntry("/InternalOrderSet", {
+				"groupId": "createGroup",
+				properties: {
+					"OrderType": this.getView().byId("AUART").getValue(),
+					"Description": this.getView().byId("AUFTEXT").getValue(),
+					"CompanyCode": this.getView().byId("BUKRS").getValue(),
+					"BusinessArea": this.getView().byId("GSBER").getValue(),
+					"Plant": this.getView().byId("WERKS").getValue(),
+					"ProfitCenter": this.getView().byId("PRCTR").getValue(),
+					"ResponsibleCCTR": this.getView().byId("KOSTL").getValue(),
+					"UserResponsible": this.getView().byId("BNAME").getValue(),
+					"WBSElement": this.getView().byId("PSPNR").getValue(),
+					"RequestCCTR": this.getView().byId("REQUESTCCTR").getValue(),
+					"RequestCode": this.getView().byId("REQCOD").getValue(),
+					"RequestOrder": this.getView().byId("AUFNR").getValue(),
+					// "SystemStatus": this.getView().byId().getValue(),
+					// "UserStatus": this.getView().byId().getValue(),
+					//   "StatusNumber": this.getView().byId().getValue(),
+					"Currency": this.getView().byId("WAERS").getValue(),
+					//     "OrderCategory": this.getView().byId().getValue(),
+					"ActPosCCTR": this.getView().byId("ACTPOSCCTR").getValue(),
+					//    "StatisOrder": this.getView().byId().getValue(),
+					//    "PlanIntegOrder": this.getView().byId().getValue(),
+					"ResultAnalysisKey": this.getView().byId("ABGSL").getValue(),
+					"CostingSheet": this.getView().byId("KALSM").getValue(),
+					"OverheadKey": this.getView().byId("ZSCHL").getValue(),
+					"InterestProfile": this.getView().byId("ZSCHM").getValue(),
+					"SettleCostElem": this.getView().byId("KSTAR").getValue(),
+					"CostCenter": this.getView().byId("COSTCENTER").getValue(),
+					"GLAccount": this.getView().byId("SAKNR").getValue(),
+					// "Applicant": this.getView().byId().getValue(),
+					// "Telephone1": this.getView().byId().getValue(),
+					// "PersonResponsible": this.getView().byId().getValue(),
+					// "Telephone2": this.getView().byId().getValue(),
+					// "EstimatedCost": this.getView().byId().getValue(),
+					"ProcessGroup": this.getView().byId("ABKRS").getValue(),
+					// "ApplicationDate": this.getView().byId().getValue(),
+					// "Department": this.getView().byId().getValue(),
+					// "WorkStart": this.getView().byId().getValue(),
+					// "EndWork": this.getView().byId().getValue(),
+					"InvestProfile": this.getView().byId("IVPRO").getValue(),
+					"Scale": this.getView().byId("SIZECL").getValue(),
+					"InvestReason": this.getView().byId("IZWEK").getValue(),
+					"EnvirInvestment": this.getView().byId("UMWKZ").getValue(),
+					"InvestProgram": this.getView().byId("PRNAM").getValue(),
+					// "PositionID": this.getView().byId().getValue(),
+					"AssetClass": this.getView().byId("ANLKL").getValue()
+						// "CapitalDate": this.getView().byId().getValue()
+
+				}
+			});
+			this.getView().getModel().setRefreshAfterChange(false);
+			this.getView().setBindingContext(oContext);
 		},
 
 		handleCancel: function(oEvent) {
@@ -57,9 +135,6 @@ sap.ui.define([
 			var fil = new sap.ui.model.Filter("FieldID", sap.ui.model.FilterOperator.EQ, fieldname);
 			this._valueHelpDialog.getBinding("items").filter([fil]);
 
-			// open project select dialog 
-			//		var selectDialog = this.byId("valuehelpfragment", "valueHelpSelectDialog");
-
 			var resourceBundle = this.getView().getModel("i18n").getResourceBundle();
 			this._valueHelpDialog.setTitle(formatter.getValueHelpTitle(resourceBundle, fieldname));
 			this._valueHelpDialog.open();
@@ -75,13 +150,12 @@ sap.ui.define([
 			}
 			var output = this.inputId.split("--");
 			var fieldname = output[2];
-			if(fieldname === 'AUART'){
+			if (fieldname === 'AUART') {
 				this.toggleIconTabVisibility(oSelectedItem.getTitle());
 			}
-			//	evt.sap.ui.getCore().byId("items");
 		},
 
-        	toggleIconTabVisibility: function(value){
+		toggleIconTabVisibility: function(value) {
 			var a = value.length;
 			var oIconTab = this.getView().byId("idIconTabBarNoIcons");
 			if (a > 0) {
@@ -90,14 +164,12 @@ sap.ui.define([
 				oIconTab.setVisible(false);
 			}
 		},
-		
+
 		orderTypeUpdate: function(evt) {
 			var a = evt.getParameters();
 			a = a.value;
 			this.toggleIconTabVisibility(a);
 		}
-		
-	
 
 	});
 
